36d353167d470abd1063f122c3b03e0b05210a19
Xiao Hou <contact@librefluids.com>
2023-05-30 18:37:38 +0200

    Python support

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -5,20 +5,26 @@
 -- Last Modified : 2023-05-29
 -- SPDX-License-Identifier: Apache-2.0
 
+------------------------------------------------------------------------------
+-- Fomatters
+------------------------------------------------------------------------------
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   {
     command = "black",
-    extra_args = { "--line-length", "79" },
+    extra_args = { "--line-length", "79" }, -- should be in project config
     filetypes = { "python" }
   },
   {
     name = "isort",
-    extra_args = { "--multi-line", "3", "--profile", "black" },
+    extra_args = { "--multi-line", "3", "--profile", "black" }, -- what's this
     filetypes = { "python" }
   },
 }
 
+------------------------------------------------------------------------------
+-- Linters
+------------------------------------------------------------------------------
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
   { command = "flake8", filetypes = { "python" } },
